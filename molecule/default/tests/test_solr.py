857364cf85a90c8e4fcb09a0e3466266d3cd4a2a
Luc Stroobant <luc@warp.be>
2020-11-03 20:18:23 +0100

    enable auth

--- molecule/default/tests/test_solr.py
+++ molecule/default/tests/test_solr.py
@@ -16,6 +16,14 @@ def test_solr_service(host):
 def test_solr_cores(host):
     """ Testing whether the cores have been created
     """
-    solr_cores = host.run('curl http://localhost:8983/solr/admin/cores')
+    solr_cores = host.run('curl --user fiaas:SolrRocks http://localhost:8983/solr/admin/cores')
     assert 'drupal7' in solr_cores.stdout
     assert 'drupal8' in solr_cores.stdout
+
+def test_solr_ping(host):
+    """ Testing whether the cores are online
+    """
+    d7 = host.run('curl --user d7:d7 http://localhost:8983/solr/drupal7/admin/ping')
+    assert 'OK' in d7.stdout
+    d8 = host.run('curl --user d8:d8 http://localhost:8983/solr/drupal8/admin/ping')
+    assert 'OK' in d8.stdout
